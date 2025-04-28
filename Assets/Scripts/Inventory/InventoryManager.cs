1923cba5672f28180d851d19a313a39c0e73d022
Baruska <164750314+baruskaa@users.noreply.github.com>
2025-04-29 01:35:49 +0800

    jusko po naayos ko na

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -13,7 +13,7 @@ public class InventoryManager : MonoBehaviour
     [SerializeField] private GameObject inventoryPanel;
     [SerializeField] private TextMeshProUGUI descriptionText;
     [SerializeField] private GameObject useButton;
-    [SerializeField] private GameObject examineButton;
+    //[SerializeField] private GameObject examineButton;
     public InventoryItemTwo currentItem;
 
     public void SetTextAndButton(string description, bool buttonActive)
@@ -22,12 +22,12 @@ public class InventoryManager : MonoBehaviour
         if (buttonActive)
         {
             useButton.SetActive(true);
-            examineButton.SetActive(true);
+           // examineButton.SetActive(true);
         }
         else
         {
             useButton.SetActive(false);
-            examineButton.SetActive(false);
+          //  examineButton.SetActive(false);
         }
     }
 
@@ -53,7 +53,7 @@ public class InventoryManager : MonoBehaviour
     // Start is called before the first frame update
     void OnEnable()
     {
-        
+
         MakeInventorySlots();
         Debug.Log("inventory made");
         SetTextAndButton("", false);
@@ -65,7 +65,7 @@ public class InventoryManager : MonoBehaviour
         currentItem = newItem;
         descriptionText.text = newDescriptionString;
         useButton.SetActive(isButtonUsable);
-        examineButton.SetActive(isButtonUsable);
+       // examineButton.SetActive(isButtonUsable);
     }
 
     public void UseButtonPressed()
@@ -84,114 +84,5 @@ public class InventoryManager : MonoBehaviour
             currentItem.Examine();
 
         }
-    }*/
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-
-public class InventoryManager : MonoBehaviour
-{
-
-    [Header("Inventory Information")]
-
-    [SerializeField] private GameObject inventoryUI;
-    public PlayerInventory playerInventory;
-    [SerializeField] private GameObject blankInventorySlot;
-    [SerializeField] private GameObject inventoryPanel;
-    [SerializeField] private TextMeshProUGUI descriptionText;
-    [SerializeField] private GameObject useButton;
-    public InventoryItemTwo currentItem;
-
-    public void SetTextAndButton(string description, bool buttonActive)
-    {
-        descriptionText.text = description;
-        if (buttonActive)
-        {
-            useButton.SetActive(true);
-        }
-        else
-        {
-            useButton.SetActive(false);
-        }
-    }
-
-    void MakeInventorySlots()
-    {
-        if (playerInventory)
-        {
-            for (int i = 0; i < playerInventory.myInventory.Count; i++)
-            {
-
-                if (playerInventory.myInventory[i].numberHeld > 0 ||
-                    playerInventory.myInventory[i].itemName == "Bottle")
-                {
-                    GameObject temp =
-                        Instantiate(blankInventorySlot,
-                        inventoryPanel.transform.position, Quaternion.identity);
-                    temp.transform.SetParent(inventoryPanel.transform);
-                    InventorySlot newSlot = temp.GetComponent<InventorySlot>();
-                    if (newSlot)
-                    {
-                        newSlot.Setup(playerInventory.myInventory[i], this);
-                    }
-                }
-            }
-        }
-    }
-
-    // Start is called before the first frame update
-    void OnEnable()
-    {
-        ClearInventorySlots();
-        MakeInventorySlots();
-        Debug.Log("made inventory");
-        SetTextAndButton("", false);
-    }
-
-    public void SetupDescriptionAndButton(string newDescriptionString,
-        bool isButtonUsable, InventoryItemTwo newItem)
-    {
-        currentItem = newItem;
-        descriptionText.text = newDescriptionString;
-        useButton.SetActive(isButtonUsable);
-    }
-
-    void ClearInventorySlots()
-    {
-        for (int i = 0; i < inventoryPanel.transform.childCount; i++)
-        {
-            Destroy(inventoryPanel.transform.GetChild(i).gameObject);
-        }
-    }
-
-    public void UseButtonPressed()
-    {
-        if (currentItem)
-        {
-            currentItem.Use();
-            //Clear all of the inventory slots
-            ClearInventorySlots();
-            //Refill all slots with new numbers
-            MakeInventorySlots();
-            if (currentItem.numberHeld == 0)
-            {
-                SetTextAndButton("", false);
-            }
-        }
-    }
-
-    public void Show()
-    {
-        inventoryUI.SetActive(true);
-        //animator.Play("show");
-    }
-
-    public void Hide()
-    {
-        inventoryUI.SetActive(false);
-        //animator.Play("hide");
-
     }
 }
